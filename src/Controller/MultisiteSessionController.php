9d97000b83be108096529c774835c40a03cc3994
Luis SEVERINO <lfs.severino@gmail.com>
2016-12-16 02:35:48 +0100

    Improve Multisite Session Main Controller

--- src/Controller/MultisiteSessionController.php
+++ src/Controller/MultisiteSessionController.php
@@ -2,18 +2,19 @@
 
 namespace \Drupal\multisite_session\Controller;
 
+use Drupal\Core\Controller\ControllerBase;
 use \Symfony\Component\HttpFoundation\Request;
 
 /**
  * Multisite Session Main Controller.
  */
-class MultisiteSessionController {
+class MultisiteSessionController extends ControllerBase {
 
   /**
    * Multisite Session Main Action.
    */
   public function indexAction(Request $request) {
-    return 'ok';
+    return 'Multisite Session Administration Page';
   }
 
 }
