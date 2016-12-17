c3ba4f3b7d20a586a4fbd94c95960057e4e511ad
Luis SEVERINO <lfs.severino@gmail.com>
2016-12-17 02:37:08 +0100

    Improve namespaces

--- src/Controller/MultisiteSessionController.php
+++ src/Controller/MultisiteSessionController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace \Drupal\multisite_session\Controller;
+namespace Drupal\multisite_session\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use \Symfony\Component\HttpFoundation\Request;
